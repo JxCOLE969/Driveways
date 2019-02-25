bb9786c929ef7f2cd0edcd5adeb2ee89d721ab6c
JxCole969 <jdc152@zips.uakron.edu>
2019-02-25 10:24:03 -0500

    adding Hibernate

---
+++ DrivewaysBackend/src/main/java/practice/Playground.java
@@ -0,0 +1,29 @@
+package practice;
+
+import org.hibernate.SessionFactory;
+
+import entity.User;
+import util.HibernateUtil;
+
+
+public class Playground {
+	
+	
+	public static SessionFactory sf = HibernateUtil.getSessionFactory();
+	
+	public static void main(String[] args) {
+		
+		startup();
+		sf.close();
+		
+	}
+	
+	public static void startup() {
+		User Test = new User();
+		Test.setUsername("Test1");
+		Test.setPhone("5555555555");
+			
+	}
+
+}
+
