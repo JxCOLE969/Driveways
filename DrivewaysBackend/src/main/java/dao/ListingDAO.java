2238cfe6e7307c0f4a6ed249614e8ddc3068bbc1
JxCole969 <jdc152@zips.uakron.edu>
2019-02-25 14:15:57 -0500

    Adding DAO and Interface Classes for Hibernate

---
+++ DrivewaysBackend/src/main/java/dao/ListingDAO.java
@@ -0,0 +1,85 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import entity.Listing;
+import util.HibernateUtil;
+
+public class ListingDAO implements IListing {
+
+	public static SessionFactory sf = HibernateUtil.getSessionFactory();
+
+	public Listing getListing(int lid) {
+
+		try {
+			Session sess = sf.openSession();
+			Listing list = sess.get(Listing.class, lid);
+			sess.close();
+
+			return list;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public boolean addOrUpdateListing(Listing l) { // Also used for adding listings as well.
+
+		try {
+
+			Session sess = sf.openSession();
+
+			sess.saveOrUpdate(l);
+
+			sess.close();
+
+			return true;
+
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
+	public boolean deleteListing(int lid) {
+
+		try {
+			Session sess = sf.openSession();
+			Listing list = sess.get(Listing.class, lid);
+			sess.beginTransaction();
+			sess.delete(list);
+			sess.getTransaction().commit();
+			sess.close();
+
+			return true;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+	public List<Listing> allListings() {
+
+		try {
+
+			Session sess = sf.openSession();
+
+			Criteria crit = sess.createCriteria(Listing.class);
+
+			List<Listing> l = crit.list();
+
+			return l;
+
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+}
--- DrivewaysBackend/src/main/java/practice/Playground.java
+++ DrivewaysBackend/src/main/java/practice/Playground.java
@@ -5,25 +5,22 @@ import org.hibernate.SessionFactory;
 import entity.User;
 import util.HibernateUtil;
 
-
 public class Playground {
-	
-	
+
 	public static SessionFactory sf = HibernateUtil.getSessionFactory();
-	
+
 	public static void main(String[] args) {
-		
+
 		startup();
 		sf.close();
-		
+
 	}
-	
+
 	public static void startup() {
 		User Test = new User();
 		Test.setUsername("Test1");
 		Test.setPhone("5555555555");
-			
+
 	}
 
 }
-
