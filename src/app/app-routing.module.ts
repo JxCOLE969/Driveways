9bd4b68f7bdf986eb1b146e15776865cf0ba74ca
Max Lettenberger <maxlettenberger@gmail.com>
2019-02-26 11:48:47 -0500

    should be a sap now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [{ path: 'signup', component: SignupComponent }];
+const routes: Routes = [{ path: 'signup', component: SignupComponent }, {path: 'home', component: AppComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
