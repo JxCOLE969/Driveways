93e5b7fe666e765da503b2586f23dd5f52bf02ff
JxCole969 <jdc152@zips.uakron.edu>
2019-02-28 19:34:16 -0500

    Added some pizzazzle to the Angular Front End..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { CreateListingComponent } from './components/create-listing/create-listing.component';
+import { RenterHomeComponent } from './components/renter-home/renter-home.component';
+import { ListerHomeComponent } from './components/lister-home/lister-home.component';
 
 const routes: Routes = [
-  { path: 'signup', component: SignupComponent }, 
-  { path: 'home', component: AppComponent }, 
-  { path: 'create-listing', component: CreateListingComponent }
+  { path: 'Signup', component: SignupComponent }, 
+  { path: '', redirectTo: 'Signup', pathMatch:'full' }, 
+  { path: 'create-listing', component: CreateListingComponent },
+  { path: 'Renter', component: RenterHomeComponent },
+  { path: 'Lister', component: ListerHomeComponent }
 ];
 
 @NgModule({
