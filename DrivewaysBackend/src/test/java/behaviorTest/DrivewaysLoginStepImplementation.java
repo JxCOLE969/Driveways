81d8e3e9ae42a87b1ce17774da215af6d35ababe
RyanSchlientz <ryan.schlientz@bobcats.gcsu.edu>
2019-03-03 21:58:37 -0500

    added step implementation for login

--- DrivewaysBackend/src/test/java/behaviorTest/DrivewaysLoginStepImplementation.java
+++ DrivewaysBackend/src/test/java/behaviorTest/DrivewaysLoginStepImplementation.java
@@ -1,61 +1,84 @@
 package behaviorTest;
 
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import junit.framework.Assert;
+import pages.DrivewayMain;
 
 public class DrivewaysLoginStepImplementation 
 {
+	static
+	{
+		File file = new File("src/test/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+	}
+	
+	static ChromeDriver driver = new ChromeDriver();
+	static DrivewayMain drivewaymain = new DrivewayMain(driver);
+	
 	@Given("^: The user is on the landing page$")
 	public void the_user_is_on_the_landing_page() throws Throwable 
 	{
-
+		driver.get("http://localhost:4200/Signup");
 	}
 
 	@When("^: The user clicks Login$")
 	public void the_user_clicks_Login() throws Throwable 
 	{
-		
+		drivewaymain.getLoginButton().click();
 	}
 
 	@Then("^: The Login modal appears$")
 	public void the_Login_modal_appears() throws Throwable 
 	{
-		
+		Assert.assertEquals(driver.findElement(By.id("loginModal")).getAttribute("style"), "display: block;");
 	}
 
-	@Given("^: The user is on the login screen$")
+	@Given("^: The user clicks Login$")
 	public void the_user_is_on_the_login_screen() throws Throwable 
 	{
-		
+		drivewaymain.getLoginButton().click();
 	}
-
-	@When("^: The user enters a correct username and password$")
-	public void the_user_enters_a_correct_username_and_password() throws Throwable 
+	
+	@When("^: The user enters a correct \"([^\"]*)\" and \"([^\"]*)\"$")
+	public void the_user_enters_a_correct_username_and_password(String un, String pw) throws Throwable 
 	{
-		
+		drivewaymain.getLoginUsername().sendKeys(un);
+		drivewaymain.getLoginPassword().sendKeys(pw);
+		drivewaymain.getLoginSubmit().click();
 	}
 
 	@Then("^: The user is signed in$")
 	public void the_user_is_signed_in() throws Throwable 
 	{
+		Assert.assertEquals(driver.findElement(By.id("loginModal")).getAttribute("style"), "display: none;");
 	}
 
-	@Given("^: The user is on the register screen$")
-	public void the_user_is_on_the_register_screen() throws Throwable 
+	@When("^: The user clicks signup$")
+	public void the_user_clicks_signup() throws Throwable 
 	{
-		
+		drivewaymain.getSignupButton().click();
 	}
 
 	@When("^: The user enters valid information$")
 	public void the_user_enters_valid_information() throws Throwable 
 	{
-		
+		drivewaymain.getSignupUsername().sendKeys("testuser");
+		drivewaymain.getSignupPassword().sendKeys("testpassword");
+		driver.findElement(By.xpath("//select[option[@value='0']]")).sendKeys("0");
+		drivewaymain.getSignupPhone().sendKeys("5555555555");
+		drivewaymain.getSignupSubmit().click();
 	}
 
 	@Then("^: The account is created$")
 	public void the_account_is_created() throws Throwable 
 	{
-		
+		Assert.assertEquals(driver.findElement(By.id("registerModal")).getAttribute("style"), "display: none;");
 	}
 }
--- DrivewaysBackend/src/test/java/pages/DrivewayMain.java
+++ DrivewaysBackend/src/test/java/pages/DrivewayMain.java
@@ -1,6 +1,8 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class DrivewayMain {
 
@@ -9,5 +11,52 @@ public class DrivewayMain {
 	public DrivewayMain(WebDriver driver) {
 		this.driver = driver;
 	}
-
+	
+	public WebElement getLoginButton() {
+		return driver.findElement(By.id("loginButton"));
+	}
+	
+	public WebElement getSignupButton() {
+		return driver.findElement(By.id("signupButton"));
+	}
+	
+	public WebElement getLoginModal() {
+		return driver.findElement(By.id("loginModal"));
+	}
+	
+	public WebElement getSignupModal() {
+		return driver.findElement(By.id("signupModal"));
+	}
+	
+	public WebElement getLoginUsername() {
+		return driver.findElement(By.id("username_login"));
+	}
+	
+	public WebElement getLoginPassword() {
+		return driver.findElement(By.id("password_login"));
+	}
+	
+	public WebElement getLoginSubmit() {
+		return driver.findElement(By.id("loginSubmit"));
+	}
+	
+	public WebElement getSignupSubmit() {
+		return driver.findElement(By.id("signupSubmit"));
+	}
+	
+	public WebElement getSignupUsername() {
+		return driver.findElement(By.id("username_register"));
+	}
+	
+	public WebElement getSignupPassword() {
+		return driver.findElement(By.id("password_register"));
+	}
+	
+	public WebElement getSignupRole() {
+		return driver.findElement(By.id("role_register"));
+	}
+	
+	public WebElement getSignupPhone() {
+		return driver.findElement(By.id("phone_register"));
+	}
 }
