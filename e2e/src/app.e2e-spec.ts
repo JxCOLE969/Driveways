f0162ab9bf22df46ef872941bc4ecda0c81792e1
Max Lettenberger <maxlettenberger@gmail.com>
2019-03-03 22:08:33 -0500

    protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,42 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+// import { AppPage } from './app.po';
+// import { browser, logging } from 'protractor';
+// import { TestComponent } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+// describe('TestComponent', () => {
+//   let component: TestComponent;
+//   let fixture: ComponentFixture<TestComponent>;
+//   let el: DebugElement;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+//   beforeEach(() => {
+//       fixture = TestBed.createComponent(TestComponent);
+//       component = fixture.componentInstance;
+//       el = fixture.debugElement;
+//       fixture.detectChanges();
+//   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to driveways!');
-  });
+//   it('should display "a new nation" as bold text', () => {
+//       const boldText = el.query(By.css('p')).nativeElement;
+//       expect(boldText.innerHTML).toContain('<b>a new nation</b>');
+//   });
+// });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    }));
-  });
-});
+// describe('workspace-project App', () => {
+//   let page: AppPage;
+
+//   beforeEach(() => {
+//     page = new AppPage();
+//   });
+
+//   it('should display welcome message', () => {
+//     page.navigateTo();
+//     expect(page.getTitleText()).toEqual('Welcome to driveways!');
+//   });
+
+//   afterEach(async () => {
+//     // Assert that there are no errors emitted from the browser
+//     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+//     expect(logs).not.toContain(jasmine.objectContaining({
+//       level: logging.Level.SEVERE,
+//     }));
+//   });
+// });
